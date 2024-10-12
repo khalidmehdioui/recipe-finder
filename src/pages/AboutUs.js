01ab959e74c38716cffad07aa90b2993778be6b2
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-10-12 12:34:42 +0100

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,25 +3,25 @@ import React from 'react';
 const AboutUs = () => {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold text-center text-[#58D68D] mb-6">About Recipe Finder</h1>
-      <p className="text-lg text-[#58D68D] mb-4">
+      <h1 className="text-4xl font-bold text-center text-[#008B8B] mb-6">About Recipe Finder</h1>
+      <p className="text-lg text-[#008B8B] mb-4">
       Recipe Finder is a user-friendly web application designed to help you discover recipes from different cuisines. 
         Whether you’re craving a classic Maroccan Pastilla, an Italian pasta, or a flavorful Mexican dish, our app provides 
         you with a variety of recipes, including ingredients and step-by-step preparation instructions.
       </p>
-      <p className="text-lg text-[#58D68D] mb-4">
+      <p className="text-lg text-[#008B8B] mb-4">
         Our goal is to make cooking at home easy, fun, and accessible. With Recipe Finder, you can explore, save, and enjoy
         delicious meals from around the world. Each recipe is carefully curated to bring you an authentic taste of diverse cuisines.
       </p>
-      <p className="text-lg text-[#58D68D] mb-4">
+      <p className="text-lg text-[#008B8B] mb-4">
         We hope Recipe Finder becomes your go-to resource for meal inspiration. Happy cooking!
       </p>
-      <p className="text-lg text-[#58D68D] mb-4">
+      <p className="text-lg text-[#008B8B] mb-4">
         Join our community of food enthusiasts and share your favorite recipes! Together, we can create a wonderful collection 
         of culinary delights that everyone can enjoy. Whether you're a novice cook or a seasoned chef, there's something for 
         everyone at Recipe Finder. Start exploring today and let your culinary adventure begin!
       </p>
-      <p className="text-lg text-[#58D68D] mb-4">
+      <p className="text-lg text-[#008B8B] mb-4">
         If you have any questions or suggestions, feel free to reach out to us through our contact page. We're always here 
         to help you with your cooking journey!
       </p>
