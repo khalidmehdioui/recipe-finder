657f432af382606236c8e9aa01877aefd21a9f67
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-10-12 12:30:38 +0100

    Update CuisinePage.js

--- src/pages/CuisinePage.js
+++ src/pages/CuisinePage.js
@@ -55,10 +55,10 @@ const CuisinePage = () => {
 
   return (
     <div className="p-8 text-center">
-      <h1 className="text-2xl font-bold mb-4 text-[#58D68D]">{cuisineType} Cuisine</h1>
+      <h1 className="text-2xl font-bold mb-4 text-[#008B8B]">{cuisineType} Cuisine</h1>
 
       {recipes.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-[#58D68D]">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 text-[#008B8B]">
           {recipes.map((recipe, index) => (
             <div key={index}>
              
