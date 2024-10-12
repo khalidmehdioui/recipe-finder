8ca5aad0c6ef1b01d4989d43c12d6b3e326c689d
khalidmehdioui <khalidmehdioui10@gmail.com>
2024-10-12 15:46:47 +0100

    Update CuisinePage.js

--- src/pages/CuisinePage.js
+++ src/pages/CuisinePage.js
@@ -6,18 +6,18 @@ const CuisinePage = () => {
   const { type: cuisineType } = useParams(); 
 
   const cuisines = {
-    Maroccan: [
+    Spanish: [
       {
-        name: "Tajin",
-        image: "https://th.bing.com/th/id/R.6b1e194d390dc23aca7f86a392214b10?rik=tZCYKhL57xKI0w&pid=ImgRaw&r=0",
+        name: "Seafood fideuà",
+        image: "https://www.themealdb.com/images/media/meals/wqqvyq1511179730.jpg",
       },
       {
-        name: "Pastilla",
-        image: "https://thumbs.dreamstime.com/b/fresh-baked-moroccan-fish-pastilla-close-up-239501736.jpg",
+        name: "Spanish Tortilla",
+        image: "https://www.themealdb.com/images/media/meals/quuxsx1511476154.jpg",
       },
       {
-        name: "Couscous",
-        image: "https://i.pinimg.com/originals/4c/53/56/4c5356b715ae1a13cc815888e0244ef6.jpg",
+        name: "Roast fennel and aubergine paella",
+        image: "https://www.themealdb.com/images/media/meals/1520081754.jpg",
       },
     ],
     Italian: [
@@ -34,18 +34,18 @@ const CuisinePage = () => {
         image: "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg",
       },
     ],
-    Mexican: [
+    Thaïlandian : [
       {
-        name: "Tacos",
-        image: "https://blog.amigofoods.com/wp-content/uploads/2020/12/tacos-authentic-mexican-food.jpg",
+        name: "Pad See Ew",
+        image: "https://www.themealdb.com/images/media/meals/uuuspp1468263334.jpg",
       },
       {
-        name: "chile relleno",
-        image: "https://th.bing.com/th/id/R.da960f9ac204694e839ec6d59909844e?rik=3qOLd07U3TKgdA&riu=http%3a%2f%2fwww.chowstatic.com%2fassets%2f2014%2f09%2f29565_chiles_rellenos-3000x2000.jpg&ehk=njcRLA8f2ybGTctO2dwRzGN7zoBWawYW3quO6G6L2Rg%3d&risl=&pid=ImgRaw&r=0",
+        name: "Thai Green Curry",
+        image: "https://www.themealdb.com/images/media/meals/sstssx1487349585.jpg",
       },
       {
-        name: "guacamole",
-        image: "https://th.bing.com/th/id/R.ec7a86e2a6417b33514a4e13b3a7dc9a?rik=O0WUSb9ik9WXrg&pid=ImgRaw&r=0",
+        name: "Massaman Beef curry",
+        image: "https://www.themealdb.com/images/media/meals/tvttqv1504640475.jpg",
       },
     ],
   };
